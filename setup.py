5aa525ed26f34705c58d62ccf8a8945d0a64bd15
tombiasz <tomasz.m.nowacki@gmail.com>
2017-08-15 16:56:10 +0200

    add url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='django-hibpwned',
     version='0.1',
     description='Django password validator based on haveibeenpwned.com API',
-    url='',
+    url='https://github.com/tombiasz/django-hibpwned',
     author='tombiasz',
     author_email='',
     license='MIT',
