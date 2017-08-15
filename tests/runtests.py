0481f8c3a60a699ad0210c14c61b23ddc66897f6
tombiasz <tomasz.m.nowacki@gmail.com>
2017-08-15 13:28:22 +0200

    fix: use proper dir for running tests

--- tests/runtests.py
+++ tests/runtests.py
@@ -6,10 +6,12 @@ import django
 from django.conf import settings
 from django.test.utils import get_runner
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests_settings'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
-    failures = test_runner.run_tests(["."])
+    failures = test_runner.run_tests([
+        os.path.dirname(os.path.realpath(__file__)),
+    ])
     sys.exit(bool(failures))
