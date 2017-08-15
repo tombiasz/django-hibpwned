b4e4f3771ebe8e4bb2d949a9c10bb94872d9f40b
tombiasz <tomasz.m.nowacki@gmail.com>
2017-08-15 16:51:56 +0200

    add password validator and tests

--- haveibeenpwned/validators.py
+++ haveibeenpwned/validators.py
@@ -1,2 +1,36 @@
-class HaveIBeenPwnedValidator():
-    pass
+import hashlib
+import requests
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext as _
+
+
+class HaveIBeenPwnedValidator(object):
+
+    HIBPWNED_API_URL = 'https://haveibeenpwned.com/api/v2/pwnedpassword/'
+    HIBPWNED_HEADERS = {
+        'User-Agent': 'Django-HIBPwned'
+    }
+
+    def _get_hash(self, password):
+        return  hashlib.sha1(password.encode('UTF-8')).hexdigest()
+
+    def validate(self, password, user=None):
+        password_hash = self._get_hash(password)
+        response = requests.post(
+            self.HIBPWNED_API_URL,
+            data = {
+                'Password': password_hash
+            },
+            headers = self.HIBPWNED_HEADERS
+        )
+        if response.status_code == requests.codes.ok:
+            raise ValidationError(
+                _("This password has previously appeared in a data breach. Please choose more secure alternative."),
+                code='password_pwned',
+            )
+
+    def get_help_text(self):
+        return _(
+            "Your password will be checked against existing data breaches."
+        )
--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,9 +1,60 @@
 from haveibeenpwned.validators import HaveIBeenPwnedValidator
+from unittest.mock import patch, Mock
 
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 
 class HaveIBeenPwnedValidatorTest(TestCase):
 
-    def test_validate(self):
-        self.fail('add tests')
+    @patch('hashlib.sha1')
+    def test_get_password_hash_uses_sha1(self, mock_sha1):
+        HaveIBeenPwnedValidator()._get_hash('test')
+        self.assertTrue(mock_sha1.called)
+
+    @patch('haveibeenpwned.validators.HaveIBeenPwnedValidator._get_hash')
+    @patch('requests.post')
+    def test_validate_not_raise_on_proper_password(self, mock_post, mock_get_hash):
+        mock_get_hash.return_value = 'testhash'
+        mock_response = Mock()
+        mock_response.status_code = 400
+        mock_post.return_value = mock_response
+
+        self.assertIsNone(HaveIBeenPwnedValidator().validate('test'))
+
+    @patch('haveibeenpwned.validators.HaveIBeenPwnedValidator._get_hash')
+    @patch('requests.post')
+    def test_validate_raise_on_proper_password(self, mock_post, mock_get_hash):
+        mock_get_hash.return_value = 'testhash'
+        mock_response = Mock()
+        mock_response.status_code = 200
+        mock_post.return_value = mock_response
+        expected_error = "This password has previously appeared in a data breach. Please choose more secure alternative."
+
+        with self.assertRaises(ValidationError) as e:
+            HaveIBeenPwnedValidator().validate('test')
+        self.assertEqual(e.exception.messages, [expected_error])
+        self.assertEqual(e.exception.error_list[0].code, 'password_pwned')
+
+    @patch('haveibeenpwned.validators.HaveIBeenPwnedValidator._get_hash')
+    @patch('requests.post')
+    def test_validate_api_called_with_proper_params(self, mock_post, mock_get_hash):
+        mock_get_hash.return_value = 'testhash'
+        mock_response = Mock()
+        mock_response.status_code = 400
+        HaveIBeenPwnedValidator().validate('test') # should not raise
+
+        self.assertTrue(mock_post.called)
+        self.assertIsNone(mock_post.assert_called_with(
+            HaveIBeenPwnedValidator.HIBPWNED_API_URL,
+            data = {
+                'Password': mock_get_hash.return_value
+            },
+            headers = HaveIBeenPwnedValidator.HIBPWNED_HEADERS
+        ))
+
+    def test_help_text(self):
+        self.assertEqual(
+            HaveIBeenPwnedValidator().get_help_text(),
+            "Your password will be checked against existing data breaches."
+        )
