d5b95847f366e3785966161ddb2e62f7393266ff
tombiasz <tomasz.m.nowacki@gmail.com>
2018-05-21 14:26:59 +0200

    use new haveibeenpwned range api

--- haveibeenpwned/validators.py
+++ haveibeenpwned/validators.py
@@ -1,5 +1,6 @@
 import hashlib
 import requests
+import urllib.parse
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
@@ -7,19 +8,33 @@ from django.utils.translation import ugettext as _
 
 class HaveIBeenPwnedValidator(object):
 
-    HIBPWNED_API_URL = 'https://api.pwnedpasswords.com/pwnedpassword/'
+    HIBPWNED_API_URL = 'https://api.pwnedpasswords.com/range/'
     HIBPWNED_HEADERS = {
         'User-Agent': 'Django-HIBPwned'
     }
 
-    def _get_hash(self, password):
-        return  hashlib.sha1(password.encode('UTF-8')).hexdigest()
+    def get_hash(self, password):
+        return  hashlib.sha1(password.encode('UTF-8')).hexdigest().upper()
+
+    def get_api_url(self, document):
+        return urllib.parse.urljoin(self.HIBPWNED_API_URL, document)
+
+    def partition_hash(self, hash):
+        return hash[:5], hash[5:]
+
+    def api_response_iter(self, content):
+        for entry in content.splitlines():
+            hash_suffix, *_ = entry.partition(b':')
+            yield hash_suffix.decode('UTF-8')
 
     def validate(self, password, user=None):
-        password_hash = self._get_hash(password)
-        url = f'{self.HIBPWNED_API_URL}{password_hash}'
+        hash = self.get_hash(password)
+        prefix, suffix = self.partition_hash(hash)
+        url = self.get_api_url(prefix)
         response = requests.get(url, headers=self.HIBPWNED_HEADERS)
-        if response.status_code == requests.codes.ok:
+
+        if (response.status_code == requests.codes.ok and
+           suffix in self.api_response_iter(response.content)):
             raise ValidationError(
                 _("This password has previously appeared in a data breach. Please choose more secure alternative."),
                 code='password_pwned',
--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,3 +1,4 @@
+import random
 from haveibeenpwned.validators import HaveIBeenPwnedValidator
 from unittest.mock import patch, Mock
 
@@ -7,51 +8,77 @@ from django.test import TestCase
 
 class HaveIBeenPwnedValidatorTest(TestCase):
 
+    def setUp(self):
+        self.validator = HaveIBeenPwnedValidator()
+
+    def test_get_password_hash_return_uppercase_string(self):
+        hash = self.validator.get_hash('test')
+        self.assertTrue(hash.isupper())
+
     @patch('hashlib.sha1')
     def test_get_password_hash_uses_sha1(self, mock_sha1):
-        HaveIBeenPwnedValidator()._get_hash('test')
+        self.validator.get_hash('test')
         self.assertTrue(mock_sha1.called)
 
-    @patch('haveibeenpwned.validators.HaveIBeenPwnedValidator._get_hash')
+    def test_get_api_url_return_proper_url(self):
+        document = 'test'
+        expected = f'{self.validator.HIBPWNED_API_URL}{document}'
+        actual = self.validator.get_api_url(document)
+        self.assertTrue(expected == actual)
+
+    def test_partion_hash_return_properly_splited_value(self):
+        expected_prefix, expected_suffix = 'aaaaa', 'bbbbbbbbbbbbbbb'
+        hash = f'{expected_prefix}{expected_suffix}'
+        actual_prefix, actual_suffix = self.validator.partition_hash(hash)
+        self.assertTrue(expected_prefix == actual_prefix)
+        self.assertTrue(expected_suffix == actual_suffix)
+
+    def test_api_response_iter_return_properly_parsed_content(self):
+        hashes = ['aaaaaaaaaa', 'bbbbbbbbbb', 'cccccccccc']
+        content_formatted = '\r\n'.join([f'{h}:1337' for h in hashes])
+        response_content = content_formatted.encode()  # must be byte string
+        returned = list(self.validator.api_response_iter(response_content))
+        self.assertTrue(hashes == returned)
+
+    @patch('haveibeenpwned.validators.HaveIBeenPwnedValidator.get_hash')
     @patch('requests.get')
     def test_validate_not_raise_on_proper_password(self, mock_get, mock_get_hash):
         mock_get_hash.return_value = 'testhash'
         mock_response = Mock()
         mock_response.status_code = 400
         mock_get.return_value = mock_response
+        self.assertIsNone(self.validator.validate('test'))
 
-        self.assertIsNone(HaveIBeenPwnedValidator().validate('test'))
-
-    @patch('haveibeenpwned.validators.HaveIBeenPwnedValidator._get_hash')
+    @patch('haveibeenpwned.validators.HaveIBeenPwnedValidator.get_hash')
     @patch('requests.get')
     def test_validate_raise_on_proper_password(self, mock_get, mock_get_hash):
-        mock_get_hash.return_value = 'testhash'
+        mock_get_hash.return_value = 'aaaaabbbbbb'
         mock_response = Mock()
         mock_response.status_code = 200
+        mock_response.content = b'bbbbbb:3\r\n'
         mock_get.return_value = mock_response
         expected_error = "This password has previously appeared in a data breach. Please choose more secure alternative."
-
         with self.assertRaises(ValidationError) as e:
-            HaveIBeenPwnedValidator().validate('test')
+            self.validator.validate('test')
         self.assertEqual(e.exception.messages, [expected_error])
         self.assertEqual(e.exception.error_list[0].code, 'password_pwned')
 
-    @patch('haveibeenpwned.validators.HaveIBeenPwnedValidator._get_hash')
+    @patch('haveibeenpwned.validators.HaveIBeenPwnedValidator.get_hash')
     @patch('requests.get')
     def test_validate_api_called_with_proper_params(self, mock_get, mock_get_hash):
         mock_get_hash.return_value = 'testhash'
         mock_response = Mock()
         mock_response.status_code = 400
-        HaveIBeenPwnedValidator().validate('test') # should not raise
-
+        hash_prefix = mock_get_hash.return_value[:5]
+        self.validator.validate('test') # should not raise
         self.assertTrue(mock_get.called)
         self.assertIsNone(mock_get.assert_called_with(
-            f'{HaveIBeenPwnedValidator.HIBPWNED_API_URL}{mock_get_hash.return_value}',
+            f'{HaveIBeenPwnedValidator.HIBPWNED_API_URL}{hash_prefix}',
             headers = HaveIBeenPwnedValidator.HIBPWNED_HEADERS
         ))
 
     def test_help_text(self):
         self.assertEqual(
-            HaveIBeenPwnedValidator().get_help_text(),
+            self.validator.get_help_text(),
             "Your password will be checked against existing data breaches."
         )
